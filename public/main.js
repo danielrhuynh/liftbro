a7fc3d74d84b8821dc3cd207ca06521fdaf43a37
Dan Huynh <50092+DHUYNH12@users.noreply.github.ford.com>
2022-11-01 20:52:14 -0400

    reconfigured electron

---
+++ public/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow} = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+require('@electron/remote/main').initialize();
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            enableRemoteModule: true,
+            nodeItegration: true
+        }
+    })
+
+    win.loadURL(
+        isDev 
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, '../build/index.html')}`
+    )
+}
+
+app.on('ready', createWindow)
+
+// MacOS listeners
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
