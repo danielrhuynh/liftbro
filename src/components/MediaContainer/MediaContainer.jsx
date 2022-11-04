5727cad26ea132890e50f113ef25d1c44e664556
Dan Huynh <50092+DHUYNH12@users.noreply.github.ford.com>
2022-11-04 00:27:31 -0400

    working skeletal tracing and fixed browser overwrite bug

--- src/components/MediaContainer/MediaContainer.jsx
+++ src/components/MediaContainer/MediaContainer.jsx
@@ -1,68 +1,49 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Button from '../Button/Button'
+import React, { useEffect, useRef } from 'react';
 import styles from './MediaContainer.module.css';
 import Webcam from 'react-webcam'
 import * as posenet from '@tensorflow-models/posenet';
-import * as tf from '@tensorflow/tfjs';
 import { drawKeypoints, drawSkeleton } from '../utils/poses';
+require('@tensorflow/tfjs-backend-webgl');
 
-const MediaContainer = ({ }) => {
+const MediaContainer = () => {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
-    const poseEstimationLoop = useRef(null);
-    const [model, setModel] = useState(null);
-    const [isPoseEstimation, setIsPoseEstimation] = useState(false)
+    // const poseEstimationLoop = useRef(null);
+    // const [model, setModel] = useState(null);
 
     useEffect(() => {
         loadPosenet();
-    }, [])
+    }, []);
 
     const loadPosenet = async () => {
-        let loadedModel = await posenet.load({
+        const loadedModel = await posenet.load({
             architecture: 'MobileNetV1',
             outputStride: 16,
             inputResolution: { width: 640, height: 480 },
             multiplier: 0.75
         });
 
-        setModel(loadedModel)
-        console.log("Posenet Model Loaded..")
+        // setModel(loadedModel); TODO: state not being set which is caused errors -> temp fix
+        setInterval(() => {
+            startPoseEstimation(loadedModel);
+        }, 100);
     };
 
-    const startPoseEstimation = () => {
-        if (webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
-            poseEstimationLoop.current = setInterval(() => {
-                const video = webcamRef.current.video;
-                const videoWidth = webcamRef.current.video.videoWidth;
-                const videoHeight = webcamRef.current.video.videoHeight;
+    const startPoseEstimation = (model) => {
+        if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
+            const video = webcamRef.current.video;
+            const videoWidth = webcamRef.current.video.videoWidth;
+            const videoHeight = webcamRef.current.video.videoHeight;
 
-                webcamRef.current.video.width = videoWidth;
-                webcamRef.current.video.height = videoHeight;
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
 
-                var tic = new Date().getTime()
-                model.estimateSinglePose(video, {
-                    flipHorizontal: false
-                }).then(pose => {
-                    var toc = new Date().getTime()
-                    // console.log(toc - tic, " ms")
-                    // console.log(tf.getBackend())
-                    // console.log(pose)
-
+            model.estimateSinglePose(video, {
+                flipHorizontal: false
+            }).then(pose => {
                 drawCanvas(pose, videoWidth, videoHeight, canvasRef);
-                });
-            }, 100);
+            });
         }
-    };
-
-    const stopPoseEstimation = () => clearInterval(poseEstimationLoop.current);
-
-    const handlePoseEstimation = () => {
-        if (isPoseEstimation)
-            stopPoseEstimation();
-        else
-            startPoseEstimation();
-
-        setIsPoseEstimation(current => !current)
     };
 
     const drawCanvas = (pose, videoWidth, videoHeight, canvas) => {
@@ -70,16 +51,16 @@ const MediaContainer = ({ }) => {
         canvas.current.width = videoWidth;
         canvas.current.height = videoHeight;
 
+        ctx.drawImage(webcamRef.current.video, 0, 0, videoWidth, videoHeight)
         drawKeypoints(pose["keypoints"], 0.5, ctx);
         drawSkeleton(pose["keypoints"], 0.5, ctx);
     };
-
 
     return (
         <div className={styles.mediaContainer}>
             <Webcam className={styles.media} ref={webcamRef} />
             <canvas className={styles.canvas} ref={canvasRef} />
-            <Button onClick={handlePoseEstimation}>Turn On</Button>
+
         </div>
     );
 };
